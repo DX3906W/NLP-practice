c9a5ca5b6f4cdf1b3c68fd51891b5bc010e973ae
Leon <34304655+DX3906W@users.noreply.github.com>
2020-02-22 01:55:40 +0100

    Show sample

--- KeyWordExtraction/TF-IDF/tf-idf.py
+++ KeyWordExtraction/TF-IDF/tf-idf.py
@@ -9,8 +9,10 @@ import numpy as np
 import re
 import pandas as pd
 
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
-class TF_IDF(object):
+class TF_IDF():
 
     def __init__(self, dataSet):
 
@@ -19,13 +21,6 @@ class TF_IDF(object):
         self.tf = []
         self.idf = []
         self.tf_idf = []
-
-        self.dataPreprocess()
-
-        self.tf()
-
-        self.idf()
-
 
     def dataPreprocess(self):
         for article in self.dataSet:
@@ -48,37 +43,49 @@ class TF_IDF(object):
 
         return tokens
 
-    def tf(self):
+    def TF(self):
         for c in self.count:
             record = {}
-            for w in c.keys:
-                record[w] = c[w] / sum(c.values)
+            for w in c.keys():
+                record[w] = c[w] / sum(c.values())
 
             self.tf.append(record)
 
-    def idf(self):
+    def IDF(self):
         for c in self.count:
             record = {}
-            for w in c.keys:
+            for w in c.keys():
                 record[w] = sum(1 for c_ in self.count if w in c_)
 
             self.idf.append(record)
 
 
     def getResult(self):
+
+        self.dataPreprocess()
+
+        self.TF()
+
+        self.IDF()
+
         for index, c in enumerate(self.count):
             record = {}
-            for k in c.keys:
+            for k in c.keys():
                 record[k] = self.tf[index][k] / self.idf[index][k]
 
             record = dict(sorted(record.items(), key=lambda x: x[1], reverse=True))
             self.tf_idf.append(record)
 
+        return self.tf_idf
+
 if __name__ == "__main__":
 
-    tf = TF_IDF(["", ""])
+    text_1 = "In information retrieval, tf–idf or TFIDF, short for term frequency–inverse document frequency, is a numerical statistic that is intended to reflect how important a word is to a document in a collection or corpus. It is often used as a weighting factor in searches of information retrieval, text mining, and user modeling. The tf–idf value increases proportionally to the number of times a word appears in the document and is offset by the number of documents in the corpus that contain the word, which helps to adjust for the fact that some words appear more frequently in general. Tf–idf is one of the most popular term-weighting schemes today; 83% of text-based recommender systems in digital libraries use tf–idf."
+    text_2 = "Variations of the tf–idf weighting scheme are often used by search engines as a central tool in scoring and ranking a document's relevance given a user query. tf–idf can be successfully used for stop-words filtering in various subject fields, including text summarization and classification."
+    text_3 = "One of the simplest ranking functions is computed by summing the tf–idf for each query term; many more sophisticated ranking functions are variants of this simple model."
 
-    tf.getResult()
+    tf = TF_IDF([text_1, text_2, text_3])
 
+    result = tf.getResult()
 
-
+    print(result)
