3470bec7c531aa92b5af56a3f5733f5bc347550c
Leon <34304655+DX3906W@users.noreply.github.com>
2020-03-05 10:25:23 +0100

    2020-03-05

--- QuestionAnswering/SpellingCorrection.py
+++ QuestionAnswering/SpellingCorrection.py
@@ -1,10 +1,29 @@
+import numpy as np
+from itertools import chain
 
 #generate all possible wrong spelling candidate
-def generateCandidate(word, threshold):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    replacement = [letters[j:j+i] for i in range(threshold) for j in range(len(letters))]
+def generate_edit_one(word):
 
-    
+    candidate = []
+
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(str[:i],str[i:]) for i in range (len(str) + 1)]
+
+    inserts = [L + c + R for L ,R in splits for c in letters]           #[a-z]apple,
+    deletes = [L + R[1:] for L,R in splits if R]
+    replaces = [L + c + R[1:] for L,R in splits if R for c in letters ]
+
+    candidate.append(list(set(inserts+deletes+replaces)))
+
+def generate_condidate(word, threshold):
+
+    candidates = {}
+    candidates[0] = generate_edit_one(word)
+
+    for i in range(1, threshold):
+        candidates[i] = [generate_edit_one(w) for w in candidates[i]]
+
+    return list(set(chain.from_iterable(candidates.keys())))
 
 #load all data
 def load_dataset():
@@ -14,8 +33,24 @@ def load_dataset():
     testdata_path = root_path + 'textdata.txt'
 
     vocab = []
-    for v in open(vocan_path, 'r'):
+    for v in open(vocab_path, 'r'):
         vocab.append(v)
 
+
+
+def channel_prob(sentence):
+
+    channel_prob = {}
+
+    for line in open('spell-errors.txt'):
+        items = line.split(":")
+        correct = items[0].strip()
+        mistakes = [item.strip() for item in items[1].strip().split(",")]
+        channel_prob[correct] = {}
+        for mis in mistakes:
+            channel_prob[correct][mis] = 1.0/len(mistakes)
+
+    return channel_prob
 #
-def correct(word):
+def correct():
+    load_dataset()
