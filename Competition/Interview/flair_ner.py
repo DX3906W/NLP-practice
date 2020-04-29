f0eb39eb4bb192a0c18b95064151175a299a8a6a
Leon <34304655+DX3906W@users.noreply.github.com>
2020-04-29 16:52:23 +0200

    Add flair ner

---
+++ Competition/Interview/flair_ner.py
@@ -0,0 +1,19 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from extract_pdf import get_data
+
+
+data = get_data()
+
+for index, file in enumerate(data):
+  doc = ' '.join(txt_data)
+  # make a sentence
+  sentence = Sentence(doc)
+  # load the NER tagger
+  tagger = SequenceTagger.load('ner')
+  tagger.predict(sentence)
+  # iterate over entities and print
+  result = sentence.get_spans('ner')
+  with open('F:/mygit/NLP-practice/Competition/Interview/output/flair_ner_'+str(index)+'.txt', 'w') as f:
+    for entity in result:
+      f.write(entity.text + ', ' + entity.tag + '\n')
